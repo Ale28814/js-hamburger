d1bbd55e285dd8086642953038636b857d556c5e
Ale28814 <alened928@gmail.com>
2021-10-25 17:16:01 +0200

    add correction

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ function closeMenu(){
 function openMenu(){
     $('.hamburger-menu').addClass('active');
 }
+
+
+
+
+/*CORREZIONE IN CLASSE*/ 
+
+/*
+    const menuToggle = document.querySelector('.header-right > a');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const closeButton = document.querySelector('.close');
+
+    menuToggle.addEventListener('click', function() {
+        hamburgerMenu.classList.add('active');
+    });
+
+    closeButton.addEventListener('click', function() {
+        hamburgerMenu.classList.remove('active');
+    });
+*/
